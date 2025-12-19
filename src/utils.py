c8c0061e4aa81a938ced6b91a63961f4fe7b6236
SHIVANG577 <2024ceb1049@iitrpr.ac.in>
2025-12-19 12:24:39 +0530

    hyperpsarameter tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.logger import logging
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV    
 
 def save_object(file_path, obj):
     try:
@@ -20,14 +21,19 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)\
         
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models,params):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
-            # Train the model
-            model.fit(X_train, y_train)
+            param=params[list(models.keys())[i]]
 
+            gs=GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+            
             # Predicting the training set
             y_train_pred = model.predict(X_train)
 
